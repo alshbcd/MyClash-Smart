b3b6391582a340c199d9103d5be4f34561075cad
alshbcd <alshbcd@163.com>
2026-09-12 16:35:16 +0800

    Update mihomoScript.js

--- Script/mihomoScript.js
+++ Script/mihomoScript.js
@@ -2,7 +2,7 @@
  * mihomo配置覆写脚本（全量版）
  * 作者：AIsouler
  * 源仓库：https://github.com/AIsouler/MyClash
- * 脚本链接：https://raw.githubusercontent.com/AIsouler/MyClash/main/Script/mihomoScript.js
+ * 脚本链接：https://raw.githubusercontent.com/alshbcd/MyClash-Smart/refs/heads/main/Script/mihomoScript.js
  * 友情推荐，非常好用、省电且内存占用低的代理软件：https://github.com/appshubcc/Bettbox
  */
 
